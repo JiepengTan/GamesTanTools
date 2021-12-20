2fc61973a6d27f155c7d7f7b54dc3653b59c3a78
jiepengtan <jiepengtan@gmail.com>
2021-12-20 21:00:15 +0800

    [+] Init Commit

---
+++ GamesTan.Util/Utils/LUTGenerator.cs
@@ -0,0 +1,45 @@
+﻿// author : https://github.com/JiepengTan
+
+using System.IO;
+using NaughtyAttributes;
+using UnityEngine;
+
+namespace GamesTan.Util {
+    [CreateAssetMenu(menuName = "GamesTan/Config/LUTGenerator")]
+    public class LUTGenerator : ScriptableObject {
+        public bool IsAutoRefresh = true;
+        [Space]
+        [OnValueChanged("OnGradientChanged")]
+        public int Width = 4;
+        [OnValueChanged("OnGradientChanged")]
+        public int Height = 1024;
+
+        [OnValueChanged("OnGradientChanged")]
+        public Gradient Gradient = new Gradient();
+
+        private  void OnGradientChanged() {
+            if(IsAutoRefresh)
+                GenTexture();
+        }
+
+        [Button()]
+        private void GenTexture() {
+            var tex = new Texture2D(Width, Height);
+            var colors = new Color[Height * Width];
+            for (int i = 0; i < Height; i++) {
+                var val = Gradient.Evaluate(i * 1.0f / Height);
+                for (int j = 0; j < Width; j++) {
+                    colors[i * Width + j] = val;
+                }
+            }
+
+            tex.SetPixels(0, 0, Width, Height, colors);
+            tex.Apply();
+            var data = tex.EncodeToPNG();
+            var path = EditorExtUtil.GetAssetPath(this);
+            path = path.Replace(".asset", ".png");
+            File.WriteAllBytes(PathUtil.GetFullPath(path), data);
+            EditorExtUtil.ImportAsset(path);
+        }
+    }
+}
---
+++ GamesTan.Util/Utils/PathUtil.cs
@@ -0,0 +1,119 @@
+﻿// author : https://github.com/JiepengTan
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace GamesTan.Util {
+    public static partial class PathUtil {
+        private static string _relPath;
+        private static string RelPath {
+            get {
+                if (_relPath == null) {
+                    _relPath = Application.dataPath.Substring(0, Application.dataPath.Length - 7);
+                }
+
+                return _relPath;
+            }
+        }
+        public static void EnsureDirectoryExist(string path) {
+            if (path.StartsWith(RelPath)) {
+                path = GetFullPath(path);
+            }
+
+            if (!Directory.Exists(path)) {
+                Directory.CreateDirectory(path);
+            }
+        }
+
+        public static string GetAbsPath(string assetPath) {
+            assetPath = assetPath.Replace("\\", "/");
+            var paths = assetPath.Split('/');
+            List<string> strs = new List<string>();
+            foreach (var path in paths) {
+                if (path == ".") {
+                    continue;
+                }
+
+                if (path == "..") {
+                    try {
+                        strs.RemoveAt(strs.Count - 1);
+                    } catch (Exception e) {
+                        Console.WriteLine(e);
+                        throw;
+                    }
+
+                    continue;
+                }
+
+                strs.Add(path);
+            }
+
+            var finalPath = string.Join("/", strs);
+            return finalPath;
+        }
+
+        public static string GetFullPath(string assetPath) {
+            return Path.Combine(RelPath, assetPath);
+        }
+
+        public static string GetParentDir(string dir) {
+            dir = dir.Replace("\\", "/");
+            if (dir.EndsWith("/")) {
+                dir = dir.Substring(0, dir.Length - 1);
+            }
+
+            dir = dir.Substring(0, dir.LastIndexOf("/"));
+            return dir;
+        }
+
+        public static string GetAssetPath(string fullPath) {
+            fullPath = fullPath.Replace("\\", "/");
+            if (fullPath.StartsWith(RelPath)) {
+                return fullPath.Substring(RelPath.Length + 1);
+            }
+
+            return fullPath;
+        }
+
+        public static void Walk(string path, string exts, System.Action<string> callback, bool isIncludeDir = false,
+            bool isSaveAssets = false,
+            bool isAllDirs = true) {
+            bool isAll = string.IsNullOrEmpty(exts) || exts == "*" || exts == "*.*";
+            string[] extList = exts.Replace("*", "").Split('|');
+
+            if (Directory.Exists(path)) {
+                List<string> paths = new List<string>();
+                string[] lst = exts.Split('|');
+                foreach (var ext in lst) {
+                    // 如果选择的是文件夹
+                    SearchOption searchOption =
+                        isAllDirs ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+                    string[] files = Directory.GetFiles(path, ext, searchOption);
+                    paths.AddRange(files);
+                    if (isIncludeDir) {
+                        string[] directories = Directory.GetDirectories(path, ext, searchOption);
+                        paths.AddRange(directories);
+                    }
+                }
+
+                foreach (var item in paths) {
+                    callback?.Invoke(item);
+                }
+
+                if (isSaveAssets) { }
+            } else {
+                if (isAll) {
+                    callback?.Invoke(path);
+                } else {
+                    foreach (var ext in extList) {
+                        if (path.EndsWith(ext, StringComparison.OrdinalIgnoreCase)) {
+                            callback?.Invoke(path);
+                        }
+                    }
+                }
+                if (isSaveAssets) { }
+            }
+        }
+    }
+}
